ca3ecf56fc6e67498247e4b87c70bb0c93439203
Gutierrez <12411466@benilde.edu.ph>
2025-10-02 10:24:53 +0800

    fordemo.py, if-else.py

---
+++ if-else.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print(f"You are welcome, {name}")
+
+if age >= 18:
+    print("You are legal age")
+else:
+    print("You are not legal age")
